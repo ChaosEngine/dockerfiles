c8124690596f800f639f3f9e39ba905ea0f91cf9
Chaos <andrzej.pauli@gmail.com>
2016-08-24 22:03:28 +0200

    enhanced fluxbox with autoclose functionality, container finishes on fluxbox exit

--- oracle12c/vnc.sh
+++ oracle12c/vnc.sh
@@ -3,4 +3,4 @@
 # Remove VNC lock (if process already killed)
 rm /tmp/.X1-lock /tmp/.X11-unix/X1
 # Run VNC server with tail in the foreground
-vncserver :1 -geometry 1280x800 -depth 24 && tail -F ~/.vnc/*.log
+vncserver :1 -geometry 1280x800 -depth 24 && tail -F /root/.vnc/*.log
