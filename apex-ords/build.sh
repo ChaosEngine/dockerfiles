e35e86c4e877479f06d1264bc3a292a041fe829b
ChaosEngine <andrzej.pauli@gmail.com>
2021-10-14 19:16:06 +0200

    cleanups and addition of HTTP_PORT param

--- apex-ords/build.sh
+++ apex-ords/build.sh
@@ -2,10 +2,8 @@
 
 DOCKER=/usr/bin/docker
 
-#    --build-arg SOURCE_BRANCH="$(git rev-parse --abbrev-ref HEAD)" \
-#    --build-arg SOURCE_COMMIT="$(git rev-parse HEAD)" $dockerfile_args \
 DOCKER_BUILDKIT=1 $DOCKER build \
    --progress=auto \
     -f Dockerfile \
-    -t "chaosengine/apex-ords" .
+    -t chaosengine/apex-ords .
 
