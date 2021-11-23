ed6a6e585358c948f59b36379b8a763b59893cff
ChaosEngine <andrzej.pauli@gmail.com>
2021-11-23 21:21:53 +0100

    old ords/apex-20.2 build

--- apex-ords/build.sh
+++ apex-ords/build.sh
@@ -5,7 +5,7 @@ DOCKER=/usr/bin/docker
 DOCKER_BUILDKIT=1 $DOCKER build \
 	--progress=auto \
 	--build-arg DONT_INSTALL_PATCHSET="true" \
-	--build-arg INSTALL_APEX="false" \
+	--build-arg INSTALL_APEX="true" \
 	-f Dockerfile \
 	-t chaosengine/apex-ords .
 
