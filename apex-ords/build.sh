4f49ee8f096015ff347d022d3dd650497ed50d56
ChaosEngine <andrzej.pauli@gmail.com>
2021-12-15 00:33:32 +0100

    passing in binaries location as ARG params: eithe URL or local zips

--- apex-ords/build.sh
+++ apex-ords/build.sh
@@ -2,10 +2,19 @@
 
 DOCKER=/usr/bin/docker
 
+#Source of binary packages
+# - latest from web
+APEX_ZIP="https://download.oracle.com/otn_software/apex/apex-latest.zip"
+ORDS_ZIP="https://download.oracle.com/otn_software/java/ords/ords-latest.zip"
+# - downloaded locally with specific version (?)
+#APEX_ZIP="apex*.zip"
+#ORDS_ZIP="ords-*.zip"
+
 DOCKER_BUILDKIT=1 $DOCKER build \
 	--progress=auto \
-	--build-arg DONT_INSTALL_PATCHSET="true" \
-	--build-arg INSTALL_APEX="true" \
+	--build-arg DONT_INSTALL_PATCHSET="false" \
+	--build-arg INSTALL_APEX="false" \
+	--build-arg APEX_ZIP="$APEX_ZIP" --build-arg ORDS_ZIP="$ORDS_ZIP" \
 	-f Dockerfile \
 	-t chaosengine/apex-ords .
 
