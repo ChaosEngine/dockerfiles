8e679a431e130608ac2b2c88bf39880edf275e9b
ChaosEngine <andrzej.pauli@gmail.com>
2022-08-19 09:05:12 +0200

    apex-ords: adding EXPAND_IMAGES variable to control images extraction/usage

--- apex-ords/build.sh
+++ apex-ords/build.sh
@@ -10,11 +10,16 @@ ORDS_ZIP="https://download.oracle.com/otn_software/java/ords/ords-latest.zip"
 #APEX_ZIP="apex*.zip"
 #ORDS_ZIP="ords-*.zip"
 
+DONT_INSTALL_PATCHSET="false"
+INSTALL_APEX="false"
+EXPAND_IMAGES="false"
+
 DOCKER_BUILDKIT=1 $DOCKER build \
 	--progress=auto \
-	--build-arg DONT_INSTALL_PATCHSET="false" \
-	--build-arg INSTALL_APEX="false" \
+	--build-arg DONT_INSTALL_PATCHSET="$DONT_INSTALL_PATCHSET" \
+	--build-arg INSTALL_APEX="$INSTALL_APEX" \
 	--build-arg APEX_ZIP="$APEX_ZIP" --build-arg ORDS_ZIP="$ORDS_ZIP" \
+	--build-arg EXPAND_IMAGES="$EXPAND_IMAGES" \
 	-f Dockerfile \
 	-t chaosengine/apex-ords .
 
